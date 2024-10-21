88f87fb9bdac66f8dcfb46d2a23c5671127d50c8
devrakibulrahman <devrakibulrahman@gmail.com>
2024-10-22 00:14:43 +0600

    countries api load succesfull done on 10/22/2024 at 12:14 am

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,7 +1,17 @@
 import './Countries.css';
 import Country from '../country/Country.jsx';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Countries = () => {
+    const [country, setCountry] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+    }, []);
+
     return (
         <div>
             <Country></Country>
