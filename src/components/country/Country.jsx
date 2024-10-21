ce60475838b16d3dff87017e460d344adb175b72
devrakibulrahman <devrakibulrahman@gmail.com>
2024-10-22 03:32:12 +0600

    visited button add all country card with functionality done on 10/22/2024 at 03:32 am

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import './Country.css';
 
 const Country = ({country}) => {
 
+    const [isVisited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!isVisited);
+    };
+
     return (
-        <div className='country-card'>
+        <div className={`country-card ${isVisited ? 'inactive' : 'active'}`}>
             <div className='flag-container'>
                 <img src={country?.flags?.png} className='flag-img'/>
             </div>
             <h4>Country Name : {country.name?.common}</h4>
             <p>Country Area : {country?.area}</p>
+            <p>Country Code : {country?.cca3}</p>
             <p>Country Region : {country?.region}</p>
             <div className='btn-bx'>
-                <button>Visit</button>
+                <button onClick={handleVisited}>{isVisited ? 'Visited' : 'Visit'}</button>
                 <button>Add Visited List</button>
             </div>
         </div>
